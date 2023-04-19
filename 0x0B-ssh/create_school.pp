d41ddefba993fd9c4b1cca77ad8a3f5617f968a6
Meacha1 <meachattd@gmail.com>
2023-04-19 03:18:14 -0700

    change

---
+++ 0x0B-ssh/create_school.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp with a name school
+
+file { '~/.ssh/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
