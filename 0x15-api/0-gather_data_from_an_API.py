d828a86a79f5ba6f5c0dbf45cfe3a458d845b5b0
Meacha1 <meachattd@gmail.com>
2023-04-24 10:43:52 -0700

    todo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''Script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress
+'''
+Script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
 '''
 import requests
 import sys
@@ -9,7 +11,9 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 employee_id = sys.argv[1]
-response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+        )
 
 if response.status_code != 200:
     print(f"Error: HTTP {response.status_code}")
@@ -21,6 +25,9 @@ employee_name = todos[0]["user"]["name"]
 num_completed_todos = len(completed_todos)
 total_todos = len(todos)
 
-print(f"Employee {employee_name} is done with {num_completed_todos}/{total_todos} tasks:")
+print(
+        f"Employee {employee_name}
+        is done with {num_completed_todos}/{total_todos} tasks: "
+        )
 for todo in completed_todos:
     print(f"\t {todo['title']}")
