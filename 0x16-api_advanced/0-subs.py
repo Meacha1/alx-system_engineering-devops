948684153b16cdb2a0371485e29833fd78ad0d2e
Meacha1 <meachattd@gmail.com>
2023-05-09 13:49:23 -0700

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My-User-Agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    if response.status_code >= 300:
         return 0
+
+    return data['data']['subscribers']
