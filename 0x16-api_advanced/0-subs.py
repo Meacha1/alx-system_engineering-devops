7511dbab0a1fe3dd9519f8f5fa9eba11f8c29255
Meacha1 <meachattd@gmail.com>
2023-05-09 14:07:59 -0700

    sub_edited

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,9 @@ def number_of_subscribers(subreddit):
     import requests
 
     my_request = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+                              .format(subreddit),
+                              headers={"User-Agent": "My-User-Agent"},
+                              allow_redirects=False)
     if my_request.status_code >= 300:
         return 0
     data = my_request.json()
