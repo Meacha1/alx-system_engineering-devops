9c3ecc011b69117685559f4eb11f1aeb2335603e
Meacha1 <meachattd@gmail.com>
2023-05-09 14:31:08 -0700

    top_10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     to the subreddit"""
     import requests
 
-    my_request = requests.get("https://www.reddit.com/r/{}/about.json"
+    my_request = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
                               .format(subreddit),
                               headers={"User-Agent": "My-User-Agent"},
                               allow_redirects=False)
