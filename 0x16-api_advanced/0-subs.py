2c93f197841d7297f6af07d3b7ce3fdb4f476200
Meacha1 <meachattd@gmail.com>
2023-05-09 13:14:52 -0700

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    '''returns the number of subscribers'''
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
