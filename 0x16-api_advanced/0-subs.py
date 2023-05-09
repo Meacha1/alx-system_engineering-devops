4453f5176a3a59aa6afc8a57e4468498ca93e6f5
Meacha1 <meachattd@gmail.com>
2023-05-09 13:32:31 -0700

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,14 @@
 queries the Reddit API and returns the number of subscribers
 for a given subreddit
 '''
-import requests
 
 
 def number_of_subscribers(subreddit):
     '''returns the number of subscribers'''
+    import requests
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'My-User-Agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
