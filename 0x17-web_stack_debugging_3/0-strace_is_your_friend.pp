fca9da5f759d150e8a2efd2e01f1f65753b7321d
Meacha1 <meachattd@gmail.com>
2023-05-10 13:13:12 -0700

    pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,14 @@
-file { '/path/to/file':
+#fix Apache 500 error by fixing typo in wordpress
+file { '/var/www/html/wp-settings.php':
   ensure  => 'file',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
-  content => template('module/file.erb'),
+  content => template('module/wp-settings.erb'),
+}
+
+exec { 'restart Apache':
+  command     => '/usr/sbin/service httpd restart',
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-settings.php'],
 }
