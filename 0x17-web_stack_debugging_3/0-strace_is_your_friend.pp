8d8b1c7b6e7e5b459575137efe1f362c57b5a964
Meacha1 <meachattd@gmail.com>
2023-05-10 13:27:39 -0700

    pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
 #This Puppet manifest fixes a typo in the wp-settings.php file of a WordPress installation.
-
-file { '/var/www/html/wp-settings.php':
-  ensure  => 'file',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  content => template('module/wp-settings.erb'),
-}
-
-exec { 'restart Apache':
-  command     => '/usr/sbin/service httpd restart',
-  refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
